02ce55fd5927e0345b2d6c83eb57d2246f44b5df
Shubham Kumar <shubhamsre1997@gmail.com>
2020-10-22 16:55:36 +0530

    Updates

---
+++ precache-manifest.fb412b580b8fd13988a2856ebb156175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d9df6b977df916b9c51e95d65c17f1",
+    "url": "/school_base/index.html"
+  },
+  {
+    "revision": "77b33b581524a24c10f5",
+    "url": "/school_base/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8c888f0a9ebac10cbaef",
+    "url": "/school_base/static/css/main.36fa481b.chunk.css"
+  },
+  {
+    "revision": "77b33b581524a24c10f5",
+    "url": "/school_base/static/js/2.608a2ed4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/school_base/static/js/2.608a2ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c888f0a9ebac10cbaef",
+    "url": "/school_base/static/js/main.5a0765c9.chunk.js"
+  },
+  {
+    "revision": "ff4da2760e3471f9ad8e",
+    "url": "/school_base/static/js/runtime-main.4b2acedd.js"
+  }
+]);
